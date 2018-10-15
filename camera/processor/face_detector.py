285ad58cf86d8f9dddcc4700a2209396edc93662
sa9sha9 <sha9sa9@gmail.com>
2018-10-15 17:31:26 +0900

    Remove number of face detect feature

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -41,8 +41,6 @@ class FaceDetector(object):
         # 顔の位置を描画する
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(img, str(len(faces)), (10, 500), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # frameを戻り値として返す
         return frame
